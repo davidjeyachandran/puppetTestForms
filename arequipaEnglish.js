1cc6f1c58ed46e8779280cab4021a181c260ac72
David Jeyachandran <davidjeyachandran@gmail.com>
2021-04-15 10:29:20 -0500

    Add code for select, file and submit

--- arequipaEnglish.js
+++ arequipaEnglish.js
@@ -1,11 +1,26 @@
 const puppeteer = require("puppeteer");
 
 const fields = [
-  { label: "First name", type: "text", selector: "#edit-field-name-0-value", value: "Mat" },
-  { label: "Last name", type: "text", selector: "#edit-field-last-names-0-value", value: "Rat" },
+  { label: "First name", type: "text", selector: "#edit-field-name-0-value", value: "Matt" },
+  {
+    label: "Last name",
+    evaluate: "(selector) => document.querySelector(selector).click()",
+    type: "select",
+    selector: "#edit-field-last-names-0-value",
+    value: "Pratt",
+  },
+  { label: "Birth Year", type: "text", selector: "#edit-field-birth-year-0-value", value: "1948" },
   { label: "Phone", type: "text", selector: "#edit-field-phone-0-value", value: "955 555 555" },
   {
+    label: "English Level",
+    type: "select",
+    selector: "#edit-field-english-level",
+    value: "INTERMEDIATE",
+  },
+
+  {
     label: "Institute",
+    type: "text",
     selector: "#edit-field-institute-0-value",
     value: "Instituto de Idiomas",
   },
@@ -21,21 +36,74 @@ const fields = [
     selector: '[data-drupal-selector="edit-mail"]',
     value: "davidjeyachandran+30@gmail.com",
   },
+  {
+    label: "Picture",
+    type: "file",
+    selector: "#edit-user-picture-0-upload",
+    value: "annabelle-daddy.jpg",
+  },
+  {
+    label: "Create new account",
+    type: "submit",
+    selector: "#edit-submit",
+    value: null,
+  },
 ];
 
 (async () => {
   const testField = async (field) => {
-    console.log("Testing: " + field.label);
-    await page.waitForSelector(field.selector);
-    await page.type(field.selector, field.value);
-    await page.waitForTimeout(400);
+    try {
+      const { label, selector, type, value } = field;
+      await page.waitForSelector(selector);
+      if ("evaluate" in field && field.evaluate) await page.evaluate(field.evaluate);
+      if (type === "text") await page.type(selector, value);
+      if (type === "select") await page.select(selector, value);
+      if (type === "file") {
+        // Sets the value of the file input to fileToUpload
+        // https://stackoverflow.com/questions/59273294/how-to-upload-file-with-js-puppeteer
+        const inputUploadHandle = await page.$(selector);
+        inputUploadHandle.uploadFile(value);
+
+        /*
+        // doing click on button to trigger upload file
+        await page.waitForSelector('#upload');
+        await page.evaluate(() => document.getElementById('upload').click());
+
+        // wait for selector that contains the uploaded file URL
+        await page.waitForSelector('#upload-link');
+*/
+      }
+
+      if (type === "date") {
+        let dateString = await page.evaluate(
+          (d) =>
+            new Date(d).toLocaleDateString(navigator.language, {
+              day: "2-digit",
+              month: "2-digit",
+              year: "numeric",
+            }),
+          date.toISOString()
+        );
+        await page.type("input", dateString);
+      }
+
+      if (type === "submit") {
+        await page.click(selector);
+        await page.waitForNavigation();
+      }
+      await page.waitForTimeout(400);
+      console.log("Testing field: " + label);
+    } catch (err) {
+      console.log("Error in field: " + label);
+      console.log(err);
+    }
   };
 
   const isDebugging = () => {
     debugSettings = {
       headless: false,
       defaultViewport: null,
-      // slowMo: 250,
+      slowMo: 5,
       devtools: false,
     };
 
@@ -47,71 +115,18 @@ const fields = [
 
   const navigationPromise = page.waitForNavigation();
 
-  await page.goto("https://arequipaenglish.com/");
+  await page.goto("https://arequipaenglish.com/register");
 
   // await page.setViewport({ width: 1092, height: 764 });
 
   await navigationPromise;
 
-  await page.waitForSelector(
-    "#block-english-account-menu > .content > .clearfix > .menu-item:nth-child(1) > a"
-  );
-  await page.click(
-    "#block-english-account-menu > .content > .clearfix > .menu-item:nth-child(1) > a"
-  );
-
-  await navigationPromise;
   const title = await page.title();
   console.log("Current page: " + title + " - ", page.url());
 
   for (const key in fields) {
-    const { label, type, selector, value } = field[key][selector];
-    console.log("Processing field: " + label);
-    await page.waitForSelector(selector);
-    if (type === "text") await page.type(selector, value);
-    await page.waitForTimeout(500);
+    await testField(fields[key]);
   }
-
-  // // First name
-  // await page.waitForSelector("#edit-field-name-0-value");
-  // await page.type("#edit-field-name-0-value", "David");
-
-  // // Last name
-  // const selectorLastName = "#edit-field-last-names-0-value";
-  // await page.waitForSelector(selectorLastName);
-  // await page.type(selectorLastName, "Test");
-  // await page.waitForTimeout(500);
-
-  // const selectorPhone = "#edit-field-phone-0-value";
-  // await page.waitForSelector(selectorPhone);
-  // await page.type(selectorPhone, "997 555 555");
-  // await page.waitForTimeout(500);
-
-  await page.waitForSelector("#edit-field-institute-0-value");
-  await page.type("#edit-field-institute-0-value", "Centro de Idiomas");
-  await page.waitForTimeout(500);
-
-  await page.waitForSelector("#edit-field-degree-0-value");
-  await page.type("#edit-field-degree-0-value", "Ingeneria de Pruebas");
-  await page.waitForTimeout(500);
-
-  await page.waitForSelector('[data-drupal-selector="edit-mail"]');
-  await page.type('[data-drupal-selector="edit-mail"]', "davidjeyachandran+30@gmail.com");
-  await page.waitForTimeout(500);
-
-  // User Picture
-  await page.type("#edit-field-birth-year-0-value", "1948");
-  await page.click("#edit-user-picture-0-upload");
-
-  //   let dateString = await page.evaluate(
-  //     d => new Date(d).toLocaleDateString(navigator.language, {
-  //         day: "2-digit",
-  //         month: "2-digit",
-  //         year: "numeric",
-  //     }),
-  //     date.toISOString()
-  // );
-  // await page.type("input", dateString)")
 
   // await browser.close();
 })();
